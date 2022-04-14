0c63d078dbddd13ba5ec492b1fc465f1b9a80ac8
itzankan <akmdgamer1290@gmail.com>
2022-04-14 15:39:12 +0530

    erwer

---
+++ js/nav.js
@@ -0,0 +1,13 @@
+var ham = document.querySelector(".hamburger")
+ham.onclick = exp;
+function exp() {
+    // const bar1 = document.querySelector(".bar1")
+    // const bar2 = document.querySelector(".bar2")
+    // const bar3 = document.querySelector(".bar3")
+    // bar2.classList.toggle("ch1")
+    // bar1.classList.toggle("ch2")
+    // bar3.classList.toggle("ch3")
+    // ham.classList.toggle("ch4")
+    var nav = document.querySelector(".nav")
+    
+}
